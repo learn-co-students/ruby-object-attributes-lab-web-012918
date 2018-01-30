a0f66b1d39c68b65bf07cc63e718c558ada7de72
alin684 <al684@cornell.edu>
2018-01-30 16:52:10 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(nameinput)
+    @name = nameinput
+  end
+  def job
+    @job
+  end
+
+  def job=(jobinput)
+    @job = jobinput
+  end
+end
